8616b64411d5c2cc4c907ddef3a54aa5f741f6b7
Carcharodon80 <romanakulov1980@gmail.com>
2025-02-17 01:41:31 +0300

    Implemented MealRestController

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,17 +1,29 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.*;
+
+@Repository
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> mealsMap = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
 
     {
         MealsUtil.meals.forEach(this::save);
@@ -19,28 +31,40 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal) {
+        log.info("save {}", meal);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
             mealsMap.put(meal.getId(), meal);
             return meal;
         }
         // handle case: update, but not present in storage
+        checkMealAndUserId(meal, SecurityUtil.authUserId());
         return mealsMap.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
     @Override
     public boolean delete(int id) {
-        return mealsMap.remove(id) != null;
+        log.info("delete {}", id);
+        Meal meal = mealsMap.get(id);
+        checkMealAndUserId(meal, SecurityUtil.authUserId());
+        return meal.getUserId() == SecurityUtil.authUserId() && mealsMap.remove(id) != null;
     }
 
     @Override
     public Meal get(int id) {
-        return mealsMap.get(id);
+        log.info("get {}", id);
+        Meal meal = mealsMap.get(id);
+        checkMealAndUserId(meal, SecurityUtil.authUserId());
+        return meal.getUserId() == SecurityUtil.authUserId() ? meal : null;
     }
 
     @Override
     public Collection<Meal> getAll() {
-        return mealsMap.values();
+        log.info("getAll");
+        return mealsMap.values().stream().
+                filter(meal -> meal.getUserId() == SecurityUtil.authUserId()).
+                sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,40 +6,52 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
+    private final Map<Integer, User> usersMap = new ConcurrentHashMap<>();
+    private final AtomicInteger count = new AtomicInteger(0);
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return usersMap.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if (user.isNew()) {
+            user.setId(count.incrementAndGet());
+            usersMap.put(user.getId(), user);
+            return user;
+        }
+        return usersMap.computeIfPresent(user.getId(), (id, existingUser) -> user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return usersMap.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        List<User> users = new ArrayList<>(usersMap.values());
+        users.sort(Comparator.comparing(User::getName).thenComparing(User::getEmail));
+        return users;
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        return usersMap.values().stream().filter(user -> user.getEmail().equals(email)).findFirst().orElse(null);
     }
 }
